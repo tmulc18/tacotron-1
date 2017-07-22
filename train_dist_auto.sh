3d420d9f2ade047b250dc29c21dccb76901528d7
Thomas Mulc <tmulc@expedia.com>
2017-07-22 20:34:12 +0900

    upping the number of workers per machine to 5

--- train_dist_auto.sh
+++ train_dist_auto.sh
@@ -3,9 +3,9 @@ n=$1
 export CUDA_VISIBLE_DEVICES=-1
 python train_dist.py --job_name "ps" --task_index $n &
 
-for i in {0..3}
+for i in {0..4}
 do
 	export CUDA_VISIBLE_DEVICES=$i
-	t=$(($n * 4 + $i))
+	t=$(($n * 5 + $i))
 	python train_dist.py --job_name "worker" --task_index $t &
 done

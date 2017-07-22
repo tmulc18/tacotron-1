242e4af58d2324cedff11c5983a401b783c95fb5
Thomas Mulc <tmulc@expedia.com>
2017-07-22 19:35:45 +0900

    correct ips

--- hyperparams.py
+++ hyperparams.py
@@ -55,7 +55,7 @@ class Hyperparams:
     # Distributed computing
     # There are len(ips) machines, each with n worker nodes and 1 ps on port 2222
     n = 4
-    ips = ['35.197.40.44','35.197.40.45']
+    ips = ['35.185.240.26','104.154.152.232']
     ps = [ip+':2222' for ip in ips]
     worker = [ip+':'+str(2223+i) for ip in ips for i in range(n)]
     #ps= ['localhost:2222']

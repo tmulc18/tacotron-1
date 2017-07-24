469b048665dd6ee846bacebfd1321e51fc3d8921
Thomas Mulc <tmulc@expedia.com>
2017-07-24 13:44:46 +0900

    8 workers same machine

--- train_dist_auto.sh
+++ train_dist_auto.sh
@@ -3,9 +3,9 @@ n=$1
 export CUDA_VISIBLE_DEVICES=-1
 python train_dist.py --job_name "ps" --task_index $n &
 
-for i in {0..3}
+for i in {0..7}
 do
 	export CUDA_VISIBLE_DEVICES=$i
-	t=$(($n * 4 + $i))
+	t=$(($n * 8 + $i))
 	python train_dist.py --job_name "worker" --task_index $t &
 done
